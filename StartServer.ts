a9077735b7f0568ad1d4e6b6f6ef523857645797
matanya.asis288 <matanya.asis288@gmail.com>
2021-03-01 00:02:53 +0200

    3/1

--- StartServer.ts
+++ StartServer.ts
@@ -1,63 +1,60 @@
-import * as DQ from "./DataQuery"; 
+import {makeCommand} from "./Server"; 
+import * as svr from "./Server"; 
 
-/* Start the server */
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const exp = express()
 
-const server = express()
-
-server.use(cors({
+exp.use(cors({
     'allowedHeaders': ['Content-Type'],
     'origin': '*',
     'preflightContinue': true
 }));
-
-server.use( bodyParser.json());       
-server.use(bodyParser.urlencoded({    
+exp.use(bodyParser.json());       
+exp.use(bodyParser.urlencoded({    
   extended: true
 })); 
 
 
-/* Construct DataQuery object */
-let dataQuery = new DQ.DataQuery()
+/*************************************************************************************************
+*  Construct the serverObj object
+**************************************************************************************************/
+let server = new svr.Server()
 
-server.get("/store", (req, res) => {
-    if(req.query.query !== undefined){
-        console.log("Server: GET " + JSON.stringify(req.query))   
+/*************************************************************************************************
+*  Handle get requests
+**************************************************************************************************/
+exp.get("/api/v1/stats", (req, res) => {
+    console.log("Server: GET stats")   
+    try{
+        server.getHandler(makeCommand('stats',''), res)
+        res.status(200)
+    }
+    catch(e){
+        res.send(e.message)
+    }      
+})
+
+exp.get("/api/v1/similar", (req, res) => {
+    console.log(req.query)
+    if(req.query.word !== undefined){
+        console.log("Server: GET similar?word=" + req.query.word)   
         try{
-            res.send(dataQuery.GET(req.query.query))
-            res.status(200)
-            console.log("Server: RETURN " + dataQuery.GET(req.query.query))
+            server.getHandler(makeCommand('similar', req.query.word), res)
         }
         catch(e){
             res.send(e.message)
         }      
     }
     else{
-        res.send("ERROR: you must provide post or query")
+        res.send("ERROR: you must provide word")
     }
 })
 
-server.post("/store", (req, res) => {
-    if(req.body !== undefined){
-        console.log("Server: POST " + JSON.stringify(req.body))
-        try{
-            dataQuery.POST(JSON.stringify(req.body))
-            res.sendStatus(200)
-        }
-        catch(e){
-            res.status(500)
-            res.send(e.message)
-        }
-        
-    }
-    else{
-        res.status(500)
-        res.send("ERROR: you must provide json")
-    }
-})
-
-server.listen(5000, () => {
-    console.log("Server: Started!")
+/*************************************************************************************************
+* Start listening on the given port
+**************************************************************************************************/
+exp.listen(5000, () => {
+    console.log("Server Started!")
 })
